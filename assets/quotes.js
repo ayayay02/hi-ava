2e140d2e5fa6d50a687b141beebee963ac306e0c
ayayay02 <61862407+ayayay02@users.noreply.github.com>
2020-05-07 15:06:44 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,9 +1,9 @@
 // prettier-ignore
 export default [
     { author: "sepimagnet", quotes: "Whatever happens, keep breathing" },
-    { author: "sepimagnet", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "sepimagnet", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "sepimagnet", quotes: "I love you." },
+    { author: "sepimagnet", quotes: "Missing you is something that comes in waves" },
+    { author: "sepimagnet", quotes: "Tanggal merah sekalian aku nggak libur untuk mikirin kamu." },
+    { author: "sepimagnet", quotes: "I miss you." },
     { author: "sepimagnet", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "sepimagnet", quotes: "I love you." },
     { author: "sepimagnet", quotes: "I'm sorry for loving you." },
