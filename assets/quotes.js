825b57f97d769a63e5653fa4a6812119de6a8e9f
ayayay02 <61862407+ayayay02@users.noreply.github.com>
2020-05-20 20:37:56 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,8 +1,5 @@
 // prettier-ignore
 export default [
-    { author: "sepimagnet", quotes: "Whatever happens, keep breathing" },
-    { author: "sepimagnet", quotes: "Missing you is something that comes in waves" },
-    { author: "sepimagnet", quotes: "Tanggal merah sekalian aku nggak libur untuk mikirin kamu." },
     { author: "sepimagnet", quotes: "I miss you." },
     { author: "sepimagnet", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "sepimagnet", quotes: "I love you." },
